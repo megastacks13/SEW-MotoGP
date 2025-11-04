9e8ad4d3423ab21f8b60a8902ac7ba14a9c6da1e
megastacks13 <jai.al.fer.2016@gmail.com>
2025-11-04 15:02:21 +0100

    Acabada prac 7

--- js/memoria.js
+++ js/memoria.js
@@ -5,11 +5,29 @@ class Memoria {
         this.tableroBloqueado = true;
         this.primera_carta = null;
         this.segunda_carta = null;
+
+        this.barajarCartas()
+        this.tableroBloqueado = false;
+
+        this.cronometro = new Cronometro();
+        this.cronometro.arrancar()
     }
 
     // Voltea la carta añadiendo la propiedad data-estado="revelada"
     voltearCarta(card) {
-        card.dataset.estado = "revelada";
+        // Comprobamos las cartas
+        if (card.dataset.estado === "revelada" || card.dataset.estado === "volteada" || this.tableroBloqueado) return;
+
+        card.dataset.estado = "volteada";
+
+        // Almacenamos las cartas
+        if (this.primera_carta) {
+            this.segunda_carta = card;
+            this.comprobarPareja();
+        }
+        else{
+            this.primera_carta = card;
+        }
     }
 
     barajarCartas() {
@@ -17,7 +35,7 @@ class Memoria {
         const elementos = Array.from(main.children);
 
         // Hacemos una lista que no incluya el h2
-        const [h2, ...cartas] = elementos;
+        const [h2,p,  ...cartas] = elementos;
 
         // Barajamos las cartas
         for (let i = cartas.length - 1; i > 0; i--) {
@@ -30,5 +48,55 @@ class Memoria {
         cartas.forEach(carta => main.appendChild(carta));
     }
 
+    reiniciarAtributos(){
+        this.primera_carta = null;
+        this.segunda_carta = null;
+        this.tableroBloqueado = false;
+    }
+
+    deshabilitarCartas(){
+        this.primera_carta.dataset.estado = "revelada";
+        this.segunda_carta.dataset.estado = "revelada";
+        this.reiniciarAtributos();
+        this.comprobarJuego();
+    }
+
+    comprobarJuego(){
+        let articles = document.getElementsByTagName('article');
+        for (let article of articles) {
+            if (article.dataset.estado !== "revelada") return false;
+        }
+        this.cronometro.parar();
+        return true;
+    }
+
+    cubrirCartas(){
+        // Bloqueamos el tablero
+        this.tableroBloqueado = true;
+
+        /* Con bind
+        setTimeout(this.reiniciarAtributos.bind(this), 1500);
+        this.primera_carta.dataset.estado = "";
+        this.segunda_carta.dataset.estado = "";
+        */
+
+        // Con arrow functions
+        setTimeout(() =>{
+            // Reiniciamos el estado
+            this.primera_carta.dataset.estado = "";
+            this.segunda_carta.dataset.estado = "";
+
+            // Y los atributos
+            this.reiniciarAtributos();
+        }, 1500)
+    }
+
+    comprobarPareja(){
+        // Estructura de cartas article -> h3[0], img[1]
+        let alt_primera_carta = this.primera_carta.children[1].alt
+        let alt_segunda_carta = this.segunda_carta.children[1].alt
+        // Operador ternario
+        alt_primera_carta === alt_segunda_carta ? this.deshabilitarCartas() : this.cubrirCartas();
+    }
 
 }
